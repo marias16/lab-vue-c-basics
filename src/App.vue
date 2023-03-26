29f3f45bf3fdbf7a744e50ee3e5b5437476c72b0
Maria Suarez <msuarezh@gmail.com>
2023-03-26 18:36:09 +0200

    second iteration done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
 import FooterAw from './components/FooterAw.vue'
+import { ref } from "vue";
+
+const strOne = ref('Hello ');
+const strTwo = ref('World');
+let getSum = (str, strTwo) => `${str} ${strTwo}`;
 </script>
 
 <template>
 <NavBar />
 <h1>Page</h1>
+<p>2+2</p>
+<p>{{ getSum(strOne, strTwo) }}</p>
 <FooterAw />
 </template>
 
