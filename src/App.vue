45bbe57cfdbb50187af2a596165d79d687adeeb9
Maria Suarez <msuarezh@gmail.com>
2023-03-26 19:06:43 +0200

    iteration 5 bonus done

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,15 @@ import { ref } from "vue";
 const strOne = ref('Hello ');
 const strTwo = ref('World');
 let getSum = (str, strTwo) => `${str} ${strTwo}`;
+
+//button  css logic
+let blueActive = ref(false);
+let getActive = () => blueActive.value = true;
 </script>
 
 <template>
+<div :class="{ blue : blueActive }"> This is my page! </div>
+<button @click="getActive">Click Me</button>
 <NavBar />
 <h1>Page</h1>
 <p>2+2</p>
@@ -17,5 +23,14 @@ let getSum = (str, strTwo) => `${str} ${strTwo}`;
 </template>
 
 <style scoped>
+
+div {
+    background-color: green;
+    width: 100%;
+}
+
+.blue {
+    background-color: blue;
+}
 
 </style>
